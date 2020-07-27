75150130a81f2ed98eb31394e9bd64996ac3f508
ronanpnc <ronan.ogor@passerellesnumeriques.org>
2020-07-27 19:22:00 +0700

    Added the (great) contact page

---
+++ app/Controllers/ContactsController.php
@@ -0,0 +1,12 @@
+<?php namespace App\Controllers;
+
+class ContactsController extends BaseController
+{
+	public function index()
+	{
+		return "this is the contact page";
+	}
+
+	//--------------------------------------------------------------------
+
+}
